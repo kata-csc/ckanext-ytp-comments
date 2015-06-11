65c9fe059d145b8d0424daf7668c74b5702ad450
Peter Kronström <peter.kronstrom@gofore.com>
2015-06-11 10:01:21 +0300

    You can now only have one reply/edit box open at the same time

--- ckanext/ytp/comments/public/javascript/comments.js
+++ ckanext/ytp/comments/public/javascript/comments.js
@@ -1,3 +1,9 @@
 function ShowCommentForm(id){
-    $("#" + id).toggleClass('hidden');
+    var current = $("#" + id);
+
+    // close all other open 'togglable' forms, except the currently clicked
+    $(".togglable").not(current).addClass('hidden');
+
+    // click the current form link to toggle visibility
+    current.toggleClass('hidden');
 }
