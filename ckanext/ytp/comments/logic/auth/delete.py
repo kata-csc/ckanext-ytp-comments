1e9ce1f7ef3eba29a92176883c6af5c97abdc43a
Peter Kronström <peter.kronstrom@gofore.com>
2015-06-11 15:48:11 +0300

    Fix bug on non-admin users not being able to delete comments in their own datasets

--- ckanext/ytp/comments/logic/auth/delete.py
+++ ckanext/ytp/comments/logic/auth/delete.py
@@ -18,13 +18,14 @@ def comment_delete(context, data_dict):
     if new_authz.is_sysadmin(user):
         return {'success': True}
 
+
     cid = logic.get_or_bust(data_dict, 'id')
 
     comment = comment_model.Comment.get(cid)
     if not comment:
         return {'success': False, 'msg': _('Comment does not exist')}
 
-    if comment.user_id is not userobj.id:
+    if comment.user_id != userobj.id:
         return {'success': False, 'msg': _('User is not the author of the comment')}
 
     return {'success': True}
